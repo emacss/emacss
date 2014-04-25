96862d03cbdeadfa314f03d04cfd8bfef6de27f3
Evolve Me A Cluster of Stars <pera@ast.cam.ac.uk>
2014-04-25 12:48:07 +0100

    Updates and bug fixes 25/04/2014

--- emacss.cpp
+++ emacss.cpp
@@ -8,22 +8,30 @@
 //                                                                     //
 //  EMACSS is a numerical integrator that solves for the differential  //
 //  equations:                                                         //
-//
-//  dE/dr = e(N,r,p) = -epsilon/t_rh
+//								       //
+//  dE/dr = e(N,r,p) = -epsilon/t_rh				       //
 //                                                                     //
 //  dN/dt = f(N,r,p) = - xi*N/t_rh                                     // 
 //                                                                     //
-//  dr/dt = g(N,r,p) = (zeta - 2*xi) * r/t_rh                          //
+//  d<m>/dt = g(N,r,<m>,p) = - gamma*<m>/t_rh                          // 
+//                                                                     //
+//  dr/dt = h(N,r,p) = mu * r/t_rh                                     //
+//                                                                     // 
+//  d kappa/dt = i(kappa) = lambda * kappa/t_rh                        //
 //                                                                     // 
 //  where                                                              // 
 //                                                                     // 
 //  epsilon = f(N,r,rt,t)                                              //
 //  xi = f(N,r,rt)                                                     //
+//  mu = f(N,r,rt)                                                     //
+//  gamma = f(N,r,rt,mm,t)                                             //
+//  lambda = f(kappa,t)                                                //
 //  p is an array of parameters/variables, which define the properties //
 //  of star clusters at any given time in the life-cycle               //
 //                                                                     //
 // Equations referenced AG2012 refer to Alexander & Gieles 2012        //
-// Equations referenced AGLB2013 refer to Alexander et al. 2013        //
+// Equations referenced GALB2013 refer to Gieles et al. 2013           //
+// Equations referenced AGLB2014 refer to Alexander et al. 2014        //
 /***********************************************************************/
 
 #include "emacss.h"
@@ -37,17 +45,17 @@ int main(int argc, char* argv[]){
   
   se_module.load(&cluster,&dynamics_module);
   dynamics_module.load(&cluster,&se_module);
+
+  cluster.initialise(se_module,dynamics_module);
+  //Creates initial conditions and t=0 output.
   
-  //Sets adiabatic expansion term and core collapse time for dynamics
-  
-  //Writes initial conditions and t=0 output.
+  cluster.load(&dynamics_module,&se_module);
   cluster.output(se_module,dynamics_module); 
-  
-  while (cluster.N > 100 && cluster.time.nbody < cluster.out_time.nbody){
+
+  while (cluster.N > 100 && cluster.time < cluster.out_time){
     cluster.evolve(se_module,dynamics_module);	
     cluster.output(se_module,dynamics_module);
-  }
- 	      
+  }	      
   return 0;
 }
 
--- emacss_dir/params.cpp
+++ emacss_dir/params.cpp
@@ -3,53 +3,76 @@
   /*Contains 'fixed' data for the simulation, ie, the optimised parameters that
     are used by EMACSS to mimic the evolution of a star cluster. These are 
     designed to be user-adjustable, but are set here as a reference, and to 
-    represent optimal values recovered in AG2012 and AGLB2013.*/
+    represent optimal values recovered in AG2012, GALB2013 and AGLB2014.*/
 
 node::node(){    //Iterator for a cluster at any given time (initialisation)
 
-  frac = 0.01;
-  gamma = 0.02;
-  T_SE = 2; 
-  rhoc0 = 0.7;
+  //From AG2012 (modified for AGLB2014)
+  nvar = 13;                           //Number of parameters
+  gamma = 0.02;                        //Modified in input if required
   
-  alpha = 2.21;
-
-}
+  //From GALB2013
+  Rch0 = 0.1;
+  frac = 1e-4;
+ }
 
 stellar_evo::stellar_evo(){}
+
+dynamics::dynamics(){}
 
 void stellar_evo::load(node *innode, dynamics *indyn){
     
   mynode = innode;
   mydyn = indyn;
-
-  nu = 0.072;
+  
+  m_ref = 100;          //Sets stellar Evo properties, in Myr & Msun
+  t_ref = 3.3;
+  m_ns = 1.2;
+  t_se = 3.3;
+  
+  nu = 0.07;
   MS_1 = 4.0;
   y = -0.3;
-  z0 = 2.0;
+   
+  //From AGLB2014
+  psi1 = 8;
+  psi0 = 1.6;
 }
 
-dynamics::dynamics(){}
+void node::load(dynamics *indyn, stellar_evo *inse){
+
+  //Initialises stellar evolution
+  inse->tse(M_star,T_star);
+  //Initialises dynamical friction
+  if (galaxy.f == 1) indyn->tdf(M_star, R_star, pcMyr, T_star);
+}
 
 void dynamics::load(node *innode, stellar_evo *inse){
     
   mynode = innode;
-  myse = inse;
-
-  R1 = 0.22;
-  N1 = 1000;
+  myse = inse;  
+    
+  //General
   x = 0.75;
-  z = 2.0;
   f = 0.3;
-  k_1 = 0.25;
+   
+  if (mynode->s == 0){         //From AG2012  (modified for GALB2013)      
+  R1 = 0.145; N1 = 1.5e4; z = 1.61; xi1 = 0.0142; Fej = 0; nc = 0;
+  mynode->k1 = 0.295;      //Set in node for easier calls
+  }
+  else{                        //From AGLB2014
+  R1 = 0.22; N1 = 1000; z = 2.0; xi1 = 0.0075; Fej = 0.55; nc = 12.5;  
+  mynode->k1 = 0.24;       //Set in node for easier calls
+  }
   
-  d1 = 0.04;
-  d2 = -0.1;
-  k0 = 0.2;
-  k1 = 0.265;
-  N3 = 15000;
-  Rch0 = 0.22;
+  //From GALB2013
+  N2 = 12.0;
+  N3 = N1;
+  delta_1 = -0.09;
+  delta_2 = -0.002;
   
-  if (mynode->s == 1) {F = 15; xi0 = 0.0142; mynode->T_DYN = 1.7;}
-  else {F = 0; xi0 = 0.0142; mynode->T_DYN = 25.0;}
-}
+  //From AGLB2013
+  Y = 90;
+  b = 1.35;
+  q = 2;
+  }  
